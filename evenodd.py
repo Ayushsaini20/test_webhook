fd08e4b87b47b0b62fe8434e8c16be6609b1c5e5
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2025-04-22 15:42:21 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,18 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        return f"{number} is even"
+    else:
+        return f"{number} is odd"
+
+# Test the function with some examples
+test_numbers = [4, 7, 10, 15, 0, -3]
+for num in test_numbers:
+    result = check_even_odd(num)
+    print(result)
+
+# Alternatively, take user input
+try:
+    user_num = int(input("Enter a number to check: "))
+    print(check_even_odd(user_num))
+except ValueError:
+    print("Please enter a valid integer!")
